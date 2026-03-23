9d5db9038a965c6e7503480d3207cc78773a16ad
grood45 <grood45@gmail.com>
2026-03-23 19:13:12 +0530

    Admin Panel Optimization: Refactored Market and Profit/Loss reports for speed

--- Node Backend/controllers/admin/report/marketReport.js
+++ Node Backend/controllers/admin/report/marketReport.js
@@ -1,5 +1,4 @@
 const joi = require("joi");
-
 const mongo = require("../../../config/mongodb");
 const { getStartEndDateTime } = require("../../../utils/comman/date");
 const { SPORT_TYPE } = require("../../../constants");
@@ -11,279 +10,121 @@ const payload = {
     id: joi.string().optional(),
     to: joi.string().optional(),
     from: joi.string().optional(),
-    type: joi
-      .string()
-      .valid(
-        SPORT_TYPE.CRICKET,
-        SPORT_TYPE.SOCCER,
-        SPORT_TYPE.TENNIS,
-        SPORT_TYPE.ESOCCER,
-        SPORT_TYPE.BASKETBALL,
-        "all"
-      )
-      .optional(),
+    type: joi.string().valid(SPORT_TYPE.CRICKET, SPORT_TYPE.SOCCER, SPORT_TYPE.TENNIS, SPORT_TYPE.ESOCCER, SPORT_TYPE.BASKETBALL, "all").optional(),
   }),
 };
 
 async function handler({ body, user }) {
   const { id, limit, page, type, to, from } = body;
 
-  //   const { userId } = user
-
-  const query = {
-    gameStatus: "completed",
-  };
-
-  let reportSport = [];
-
-  const total = {
-    matchPl: 0, // odds
-    matchStack: 0, // odds
-    bookMakerStack: 0, // bookMaker
-    bookMakerPl: 0, // bookMaker
-    fancyStack: 0, //  fancy
-    fancyPl: 0, //  fancy
-    premPl: 0, // prem
-    premStack: 0, // prem
-    total: 0, // total
-  };
-
+  const query = { gameStatus: "completed" };
   if (id) query.userId = mongo.ObjectId(id);
   if (type && type !== "all") query.type = type;
-
   if (to && from) {
     const { endDate, startDate } = getStartEndDateTime(from, to);
-    query.createdAt = {
-      $gte: startDate,
-      $lte: endDate,
-    };
+    query.createdAt = { $gte: startDate, $lte: endDate };
   }
 
-  const sportsInfo = await mongo.bettingApp
-    .model(mongo.models.sports)
-    .paginate({
-      query,
-      page,
-      limit,
-      select:{
-        _id: 1,
-        name: 1,
-      }
-    });
+  const sportsInfo = await mongo.bettingApp.model(mongo.models.sports).paginate({
+    query, page, limit, select: { _id: 1, name: 1 }
+  });
 
-  for await (const element of sportsInfo.results) {
-    const betsHistory = await mongo.bettingApp
-      .model(mongo.models.betsHistory)
-      .find({
-        query: {
-          matchId: element._id,
-          winner: { $ne: "" },
-        },
-        select:{
-          _id: 1,
-          betType: 1,
-          winner: 1,
-          selection: 1,
-          betSide: 1,
-          profit: 1,
-          exposure: 1,
-          betPlaced: 1,
-          stake: 1,
-          fancyYes: 1,
-          fancyNo: 1,
-          subSelection: 1,
-        }
-      });
-    const staticData = {
-      uid: element.name, // match name,
-      matchPl: 0, // odds
-      matchStack: 0, // odds
-      bookMakerStack: 0, // bookMaker
-      bookMakerPl: 0, // bookMaker
-      fancyStack: 0, //  fancy
-      fancyPl: 0, //  fancy
-      premPl: 0, // prem
-      premStack: 0, // prem
-      total: 0, // total
-    };
-    // if (bet.winner !== "" && bet.winner !== "cancel") {
-    //   if (bet.selection === bet.winner) {
-    //   } else if (bet.selection !== bet.winner) {
-    //   }
-    // }
-    for await (const bet of betsHistory) {
+  const results = sportsInfo.results || [];
+  if (results.length === 0) {
+    return { report: { results: [], page, limit, totalPages: 0, totalResults: 0 }, total: {}, msg: "market report!" };
+  }
+
+  const matchIds = results.map(r => r._id);
+  const allBets = await mongo.bettingApp.model(mongo.models.betsHistory).find({
+    query: { matchId: { $in: matchIds }, winner: { $ne: "" } },
+    select: { matchId: 1, betType: 1, winner: 1, selection: 1, betSide: 1, profit: 1, exposure: 1, betPlaced: 1, stake: 1, fancyYes: 1, fancyNo: 1, subSelection: 1 }
+  });
+
+  const matchBetsMap = {};
+  allBets.forEach(bet => {
+    const mid = bet.matchId.toString();
+    if (!matchBetsMap[mid]) matchBetsMap[mid] = [];
+    matchBetsMap[mid].push(bet);
+  });
+
+  const total = { matchPl: 0, matchStack: 0, bookMakerStack: 0, bookMakerPl: 0, fancyStack: 0, fancyPl: 0, premPl: 0, premStack: 0, total: 0 };
+
+  const reportSport = results.map(element => {
+    const mid = element._id.toString();
+    const betsHistory = matchBetsMap[mid] || [];
+    const staticData = { uid: element.name, matchPl: 0, matchStack: 0, bookMakerStack: 0, bookMakerPl: 0, fancyStack: 0, fancyPl: 0, premPl: 0, premStack: 0, total: 0 };
+
+    betsHistory.forEach(bet => {
       if (bet.betType === "odds" || bet.betType === "bookMark") {
         if (bet.winner !== "" && bet.winner !== "cancel") {
           if (bet.selection === bet.winner) {
-            if (bet.betSide === "lay") {
-              bet.profit = bet.exposure;
-            }
-            // if (bet.betSide === "back") {
-            staticData.matchPl += bet.profit;
-            staticData.total += bet.profit;
-            // } else {
-            //   staticData.matchPl -= bet.profit;
-            //   staticData.total -= bet.profit;
-            // }
-          } else if (bet.selection !== bet.winner) {
-            if (bet.betSide === "back") {
-              bet.profit = bet.exposure;
-            }
-            // if (bet.betSide === "back") {
-            staticData.matchPl -= bet.betPlaced;
-            staticData.total -= bet.betPlaced;
-            // } else {
-            //   staticData.matchPl += bet.betPlaced;
-            //   staticData.total += bet.betPlaced;
-            // }
-          }
-          staticData.matchStack += bet.stake;
-        }
-      }
-      // else if (bet.betType === "bookMark") {
-      //   if (bet.winner !== "" && bet.winner !== "cancel") {
-      //     if (bet.selection === bet.winner) {
-      //       if (bet.betSide === "back") {
-      //         staticData.bookMakerPl += bet.profit;
-      //         staticData.total += bet.profit;
-      //       } else {
-      //         staticData.bookMakerPl -= bet.profit;
-      //         staticData.total -= bet.profit;
-      //       }
-      //       // staticData.bookMakerStack += bet.stake;
-      //     } else if (bet.selection !== bet.winner) {
-      //       if (bet.betSide === "back") {
-      //         staticData.bookMakerPl -= bet.betPlaced;
-      //         staticData.total -= bet.betPlaced;
-      //       } else {
-      //         staticData.bookMakerPl += bet.betPlaced;
-      //         staticData.total += bet.betPlaced;
-      //       }
-      //     }
-      //     staticData.bookMakerStack += bet.stake;
-      //   }
-      // }
-      else if (bet.betType === "session") {
-        if (bet.fancyYes === bet.fancyNo) {
-          if (bet.betSide === "yes") {
-            if (bet.fancyYes > Number(bet.winner)) {
-              //lost
-              staticData.fancyPl -= bet.betPlaced;
-              staticData.total -= bet.betPlaced;
-            } else {
-              staticData.fancyPl += bet.profit;
-              staticData.total += bet.profit;
-            }
+            if (bet.betSide === "lay") bet.profit = bet.exposure;
+            staticData.matchPl += (bet.profit || 0);
+            staticData.total += (bet.profit || 0);
           } else {
-            if (bet.fancyNo > Number(bet.winner)) {
-              // win
-              staticData.fancyPl += bet.betPlaced;
-              staticData.total += bet.betPlaced;
-            } else {
-              staticData.fancyPl -= bet.profit;
-              staticData.total -= bet.profit;
-            }
+            if (bet.betSide === "back") bet.profit = bet.exposure;
+            staticData.matchPl -= (bet.betPlaced || 0);
+            staticData.total -= (bet.betPlaced || 0);
           }
-          staticData.fancyStack += bet.stake;
+          staticData.matchStack += (bet.stake || 0);
+        }
+      } else if (bet.betType === "session") {
+        const isSameFancy = (bet.fancyYes === bet.fancyNo);
+        const winnerNum = Number(bet.winner);
+        if (isSameFancy) {
+          if (bet.betSide === "yes") {
+            if (bet.fancyYes > winnerNum) { staticData.fancyPl -= bet.betPlaced; staticData.total -= bet.betPlaced; }
+            else { staticData.fancyPl += bet.profit; staticData.total += bet.profit; }
+          } else {
+            if (bet.fancyNo > winnerNum) { staticData.fancyPl += bet.betPlaced; staticData.total += bet.betPlaced; }
+            else { staticData.fancyPl -= bet.profit; staticData.total -= bet.profit; }
+          }
         } else {
           if (bet.betSide === "yes") {
-            if (bet.fancyYes < Number(bet.winner)) {
-              staticData.fancyPl -= bet.betPlaced;
-              staticData.total -= bet.betPlaced;
-            } else {
-              staticData.fancyPl += bet.profit;
-              staticData.total += bet.profit;
-            }
+            if (bet.fancyYes < winnerNum) { staticData.fancyPl -= bet.betPlaced; staticData.total -= bet.betPlaced; }
+            else { staticData.fancyPl += bet.profit; staticData.total += bet.profit; }
           } else {
-            if (bet.fancyNo > Number(bet.winner)) {
-              staticData.fancyPl += bet.betPlaced;
-              staticData.total += bet.betPlaced;
-            } else {
-              staticData.fancyPl -= bet.profit;
-              staticData.total -= bet.profit;
-            }
+            if (bet.fancyNo > winnerNum) { staticData.fancyPl += bet.betPlaced; staticData.total += bet.betPlaced; }
+            else { staticData.fancyPl -= bet.profit; staticData.total -= bet.profit; }
           }
-          staticData.fancyStack += bet.stake;
         }
+        staticData.fancyStack += bet.stake;
       } else if (bet.betType === "premium") {
         if (bet.winner !== "" && !bet.winner.includes("cancel")) {
-          if (bet.winner.includes(bet.subSelection)) {
-            staticData.fancyPl += bet.profit;
-            staticData.total += bet.profit;
-          } else if (!bet.winner.includes(bet.subSelection)) {
-            staticData.fancyPl -= bet.betPlaced;
-            staticData.total -= bet.betPlaced;
-          }
+          if (bet.winner.includes(bet.subSelection)) { staticData.fancyPl += bet.profit; staticData.total += bet.profit; }
+          else { staticData.fancyPl -= bet.betPlaced; staticData.total -= bet.betPlaced; }
           staticData.fancyStack += bet.stake;
         }
       }
-    }
-    reportSport.push(staticData);
-  }
+    });
 
-  // for await (const report of reportSport) {
-  //   total.matchPl += report.matchPl;
-  //   total.matchStack += report.matchStack;
-  //   total.bookMakerStack += report.bookMakerStack;
-  //   total.bookMakerPl += report.bookMakerPl;
-  //   total.fancyStack += report.fancyStack;
-  //   total.fancyPl += report.fancyPl;
-  //   total.premPl += report.premPl;
-  //   total.premStack += report.premStack;
-  //   total.total += report.total;
-  // }
+    // Update global totals
+    total.matchPl += staticData.matchPl;
+    total.matchStack += staticData.matchStack;
+    total.bookMakerStack += staticData.bookMakerStack;
+    total.bookMakerPl += staticData.bookMakerPl;
+    total.fancyStack += staticData.fancyStack;
+    total.fancyPl += staticData.fancyPl;
+    total.premPl += staticData.premPl;
+    total.premStack += staticData.premStack;
+    total.total += staticData.total;
 
-  reportSport = reportSport.map((value) => {
-    total.matchPl += value.matchPl;
-    total.matchStack += value.matchStack;
-    total.bookMakerStack += value.bookMakerStack;
-    total.bookMakerPl += value.bookMakerPl;
-    total.fancyStack += value.fancyStack;
-    total.fancyPl += value.fancyPl;
-    total.premPl += value.premPl;
-    total.premStack += value.premStack;
-    total.total += value.total;
+    // Formatting decimals
+    ['matchPl', 'matchStack', 'bookMakerStack', 'bookMakerPl', 'fancyStack', 'fancyPl', 'premPl', 'premStack', 'total'].forEach(k => {
+      staticData[k] = Number(staticData[k].toFixed(2));
+    });
 
-    value.matchPl = Number(value.matchPl.toFixed(2));
-    value.matchStack = Number(value.matchStack.toFixed(2));
-    value.bookMakerStack = Number(value.bookMakerStack.toFixed(2));
-    value.bookMakerPl = Number(value.bookMakerPl.toFixed(2));
-    value.fancyStack = Number(value.fancyStack.toFixed(2));
-    value.fancyPl = Number(value.fancyPl.toFixed(2));
-    value.premPl = Number(value.premPl.toFixed(2));
-    value.premStack = Number(value.premStack.toFixed(2));
-    value.total = Number(value.total.toFixed(2));
-
-    return value;
+    return staticData;
   });
 
-  total.matchPl = Number(total.matchPl.toFixed(2));
-  total.matchStack = Number(total.matchStack.toFixed(2));
-  total.bookMakerStack = Number(total.bookMakerStack.toFixed(2));
-  total.bookMakerPl = Number(total.bookMakerPl.toFixed(2));
-  total.fancyStack = Number(total.fancyStack.toFixed(2));
-  total.fancyPl = Number(total.fancyPl.toFixed(2));
-  total.premPl = Number(total.premPl.toFixed(2));
-  total.premStack = Number(total.premStack.toFixed(2));
-  total.total = Number(total.total.toFixed(2));
+  ['matchPl', 'matchStack', 'bookMakerStack', 'bookMakerPl', 'fancyStack', 'fancyPl', 'premPl', 'premStack', 'total'].forEach(k => {
+    total[k] = Number(total[k].toFixed(2));
+  });
 
-  const sendObject = {
-    report: {
-      results: reportSport,
-      page: sportsInfo.page,
-      limit: sportsInfo.limit,
-      totalPages: sportsInfo.totalPages,
-      totalResults: sportsInfo.totalResults,
-    },
-    total,
-    msg: "market report!",
+  return {
+    report: { results: reportSport, page: sportsInfo.page, limit: sportsInfo.limit, totalPages: sportsInfo.totalPages, totalResults: sportsInfo.totalResults },
+    total, msg: "market report!"
   };
-
-  return sendObject;
 }
 
-module.exports = {
-  payload,
-  handler,
-  auth: true,
-};
+module.exports = { payload, handler, auth: true };
--- Node Backend/controllers/admin/report/profitLossByMarket.js
+++ Node Backend/controllers/admin/report/profitLossByMarket.js
@@ -1,5 +1,4 @@
 const joi = require("joi");
-
 const mongo = require("../../../config/mongodb");
 const { getStartEndDateTime, getDate } = require("../../../utils/comman/date");
 const { SPORT_TYPE } = require("../../../constants");
@@ -11,248 +10,134 @@ const payload = {
     id: joi.string().optional(),
     to: joi.string().optional(),
     from: joi.string().optional(),
-    type: joi
-      .string()
-      .valid(
-        SPORT_TYPE.CRICKET,
-        SPORT_TYPE.SOCCER,
-        SPORT_TYPE.TENNIS,
-        SPORT_TYPE.ESOCCER,
-        SPORT_TYPE.BASKETBALL,
-        "all"
-      )
-      .optional(),
+    type: joi.string().valid(SPORT_TYPE.CRICKET, SPORT_TYPE.SOCCER, SPORT_TYPE.TENNIS, SPORT_TYPE.ESOCCER, SPORT_TYPE.BASKETBALL, "all").optional(),
     filter: joi.string().valid("all", "today", "yesterday").optional(),
   }),
 };
 
 async function handler({ body, user }) {
   const { id, limit, page, type, to, from, filter } = body;
-
   const { userId } = user;
 
-  const query = {
-    gameStatus: "completed",
-  };
-
-  let reportSport = [];
-
-  const total = {
-    downlinePl: 0, // admin win amount
-    playerPl: 0, // player win amount
-    stake: 0, // bet amount
-    commission: 0, // bet commissions
-    UpLinePl: 0, // UpLine Pl
-  };
-
+  const query = { gameStatus: "completed" };
   if (id) query.userId = mongo.ObjectId(id);
   if (type && type !== "all") query.type = type;
   if (filter && filter !== "all") {
     const { endDate, startDate } = getDate(filter);
-    query.startDate = {
-      $gte: startDate,
-      $lte: endDate,
-    };
+    query.startDate = { $gte: startDate, $lte: endDate };
   } else if (to && from) {
     const { endDate, startDate } = getStartEndDateTime(from, to);
-    query.startDate = {
-      $gte: startDate,
-      $lte: endDate,
-    };
+    query.startDate = { $gte: startDate, $lte: endDate };
   }
 
   const userIDs = await mongo.bettingApp.model(mongo.models.users).distinct({
-    field: "_id",
-    query: { whoAdd: userId },
+    field: "_id", query: { whoAdd: userId },
   });
 
-  const sportsInfo = await mongo.bettingApp
-    .model(mongo.models.sports)
-    .paginate({
-      query,
-      page,
-      limit,
-      select: {
-        _id: 1,
-        name: 1,
-      },
-    });
+  const sportsInfo = await mongo.bettingApp.model(mongo.models.sports).paginate({
+    query, page, limit, select: { _id: 1, name: 1 },
+  });
 
-  for await (const element of sportsInfo.results) {
-    const betsHistory = await mongo.bettingApp
-      .model(mongo.models.betsHistory)
-      .find({
-        query: {
-          matchId: element._id,
-          userId: { $in: userIDs },
-          winner: { $ne: "" },
-        },
-        select: {
-          _id: 1,
-          betType: 1,
-          winner: 1,
-          selection: 1,
-          betSide: 1,
-          profit: 1,
-          exposure: 1,
-          betPlaced: 1,
-          stake: 1,
-          fancyYes: 1,
-          fancyNo: 1,
-          subSelection: 1,
-        },
-      });
-    const staticData = {
-      uid: element.name, // match name,
-      downlinePl: 0, // admin win amount
-      playerPl: 0, // player win amount
-      stake: 0, // bet amount
-      commission: 0, // bet commissions
-      UpLinePl: 0, // total
-    };
-    for await (const bet of betsHistory) {
+  const results = sportsInfo.results || [];
+  if (results.length === 0) {
+    return { report: { results: [], page, limit, totalPages: 0, totalResults: 0 }, total: {}, msg: "market report!" };
+  }
+
+  const matchIds = results.map(r => r._id);
+  const allBets = await mongo.bettingApp.model(mongo.models.betsHistory).find({
+    query: { matchId: { $in: matchIds }, userId: { $in: userIDs }, winner: { $ne: "" } },
+    select: { matchId: 1, betType: 1, winner: 1, selection: 1, betSide: 1, profit: 1, exposure: 1, betPlaced: 1, stake: 1, fancyYes: 1, fancyNo: 1, subSelection: 1 },
+  });
+
+  const matchBetsMap = {};
+  allBets.forEach(bet => {
+    const mid = bet.matchId.toString();
+    if (!matchBetsMap[mid]) matchBetsMap[mid] = [];
+    matchBetsMap[mid].push(bet);
+  });
+
+  const total = { downlinePl: 0, playerPl: 0, stake: 0, commission: 0, UpLinePl: 0 };
+
+  const reportSport = results.map(element => {
+    const mid = element._id.toString();
+    const betsHistory = matchBetsMap[mid] || [];
+    const staticData = { uid: element.name, downlinePl: 0, playerPl: 0, stake: 0, commission: 0, UpLinePl: 0 };
+
+    betsHistory.forEach(bet => {
       if (bet.betType === "odds" || bet.betType === "bookMark") {
         if (bet.winner !== "" && bet.winner !== "cancel") {
           if (bet.selection === bet.winner) {
-            // player lost
             if (bet.betSide === "lay") {
-              staticData.downlinePl += bet.profit;
-              staticData.UpLinePl += bet.profit;
-              staticData.playerPl -= bet.profit;
+              staticData.downlinePl += (bet.profit || 0);
+              staticData.UpLinePl += (bet.profit || 0);
+              staticData.playerPl -= (bet.profit || 0);
             } else {
-              // player win
-              staticData.downlinePl -= bet.profit;
-              staticData.UpLinePl -= bet.profit;
-              staticData.playerPl += bet.profit;
+              staticData.downlinePl -= (bet.profit || 0);
+              staticData.UpLinePl -= (bet.profit || 0);
+              staticData.playerPl += (bet.profit || 0);
             }
-          } else if (bet.selection !== bet.winner) {
-            // player lost
+          } else {
             if (bet.betSide === "back") {
-              staticData.downlinePl += bet.betPlaced;
-              staticData.UpLinePl += bet.betPlaced;
-              staticData.playerPl -= bet.betPlaced;
+              staticData.downlinePl += (bet.betPlaced || 0);
+              staticData.UpLinePl += (bet.betPlaced || 0);
+              staticData.playerPl -= (bet.betPlaced || 0);
             } else {
-              // player winner
-              staticData.downlinePl -= bet.betPlaced;
-              staticData.UpLinePl -= bet.betPlaced;
-              staticData.playerPl += bet.betPlaced;
+              staticData.downlinePl -= (bet.betPlaced || 0);
+              staticData.UpLinePl -= (bet.betPlaced || 0);
+              staticData.playerPl += (bet.betPlaced || 0);
             }
           }
         }
       } else if (bet.betType === "session") {
-        if (bet.fancyYes === bet.fancyNo) {
+        const isSameFancy = (bet.fancyYes === bet.fancyNo);
+        const winnerNum = Number(bet.winner);
+        if (isSameFancy) {
           if (bet.betSide === "yes") {
-            if (bet.fancyYes > Number(bet.winner)) {
-              //lost
-              staticData.downlinePl += bet.betPlaced;
-              staticData.UpLinePl += bet.betPlaced;
-              staticData.playerPl -= bet.betPlaced;
-            } else {
-              // win
-              staticData.downlinePl -= bet.profit;
-              staticData.UpLinePl -= bet.profit;
-              staticData.playerPl += bet.profit;
-            }
+            if (bet.fancyYes > winnerNum) { staticData.downlinePl += bet.betPlaced; staticData.UpLinePl += bet.betPlaced; staticData.playerPl -= bet.betPlaced; }
+            else { staticData.downlinePl -= bet.profit; staticData.UpLinePl -= bet.profit; staticData.playerPl += bet.profit; }
           } else {
-            if (bet.fancyNo > Number(bet.winner)) {
-              // win
-              staticData.downlinePl -= bet.betPlaced;
-              staticData.UpLinePl -= bet.betPlaced;
-              staticData.playerPl += bet.betPlaced;
-            } else {
-              // lost
-              staticData.downlinePl += bet.profit;
-              staticData.UpLinePl += bet.profit;
-              staticData.playerPl -= bet.profit;
-            }
+            if (bet.fancyNo > winnerNum) { staticData.downlinePl -= bet.betPlaced; staticData.UpLinePl -= bet.betPlaced; staticData.playerPl += bet.betPlaced; }
+            else { staticData.downlinePl += bet.profit; staticData.UpLinePl += bet.profit; staticData.playerPl -= bet.profit; }
           }
-          // staticData.fancyStack += bet.stake;
         } else {
           if (bet.betSide === "yes") {
-            if (bet.fancyYes < Number(bet.winner)) {
-              // won
-              staticData.downlinePl -= bet.profit;
-              staticData.UpLinePl -= bet.profit;
-              staticData.playerPl += bet.profit;
-            } else {
-              // lost
-              staticData.downlinePl += bet.betPlaced;
-              staticData.UpLinePl += bet.betPlaced;
-              staticData.playerPl -= bet.betPlaced;
-            }
+            if (bet.fancyYes < winnerNum) { staticData.downlinePl -= bet.profit; staticData.UpLinePl -= bet.profit; staticData.playerPl += bet.profit; }
+            else { staticData.downlinePl += bet.betPlaced; staticData.UpLinePl += bet.betPlaced; staticData.playerPl -= bet.betPlaced; }
           } else {
-            if (bet.fancyNo > Number(bet.winner)) {
-              // won
-              staticData.downlinePl -= bet.betPlaced;
-              staticData.UpLinePl -= bet.betPlaced;
-              staticData.playerPl += bet.betPlaced;
-            } else {
-              // lost
-              staticData.downlinePl += bet.profit;
-              staticData.UpLinePl += bet.profit;
-              staticData.playerPl -= bet.profit;
-            }
+            if (bet.fancyNo > winnerNum) { staticData.downlinePl -= bet.betPlaced; staticData.UpLinePl -= bet.betPlaced; staticData.playerPl += bet.betPlaced; }
+            else { staticData.downlinePl += bet.profit; staticData.UpLinePl += bet.profit; staticData.playerPl -= bet.profit; }
           }
-          // staticData.fancyStack += bet.stake;
         }
       } else if (bet.betType === "premium") {
         if (bet.winner !== "" && !bet.winner.includes("cancel")) {
-          if (bet.winner.includes(bet.subSelection)) {
-            staticData.downlinePl -= bet.profit;
-            staticData.UpLinePl -= bet.profit;
-            staticData.playerPl += bet.profit;
-          } else if (!bet.winner.includes(bet.subSelection)) {
-            staticData.downlinePl += bet.betPlaced;
-            staticData.UpLinePl += bet.betPlaced;
-            staticData.playerPl -= bet.betPlaced;
-          }
+          if (bet.winner.includes(bet.subSelection)) { staticData.downlinePl -= bet.profit; staticData.UpLinePl -= bet.profit; staticData.playerPl += bet.profit; }
+          else { staticData.downlinePl += bet.betPlaced; staticData.UpLinePl += bet.betPlaced; staticData.playerPl -= bet.betPlaced; }
         }
       }
+      staticData.stake += (bet.betPlaced || 0);
+    });
 
-      staticData.stake += bet.betPlaced;
-    }
-    reportSport.push(staticData);
-  }
+    total.UpLinePl += staticData.UpLinePl;
+    total.commission += staticData.commission;
+    total.downlinePl += staticData.downlinePl;
+    total.playerPl += staticData.playerPl;
+    total.stake += staticData.stake;
 
-  reportSport = reportSport.map((value) => {
-    total.UpLinePl += value.UpLinePl;
-    total.commission += value.commission;
-    total.downlinePl += value.downlinePl;
-    total.playerPl += value.playerPl;
-    total.stake += value.stake;
+    ['UpLinePl', 'commission', 'downlinePl', 'playerPl', 'stake'].forEach(k => {
+      staticData[k] = Number(staticData[k].toFixed(2));
+    });
 
-    value.UpLinePl = Number(value.UpLinePl.toFixed(2));
-    value.commission = Number(value.commission.toFixed(2));
-    value.downlinePl = Number(value.downlinePl.toFixed(2));
-    value.playerPl = Number(value.playerPl.toFixed(2));
-    value.stake = Number(value.stake.toFixed(2));
-
-    return value;
+    return staticData;
   });
 
-  total.UpLinePl = Number(total.UpLinePl.toFixed(2));
-  total.commission = Number(total.commission.toFixed(2));
-  total.downlinePl = Number(total.downlinePl.toFixed(2));
-  total.playerPl = Number(total.playerPl.toFixed(2));
-  total.stake = Number(total.stake.toFixed(2));
+  ['UpLinePl', 'commission', 'downlinePl', 'playerPl', 'stake'].forEach(k => {
+    total[k] = Number(total[k].toFixed(2));
+  });
 
-  const sendObject = {
-    report: {
-      results: reportSport,
-      page: sportsInfo.page,
-      limit: sportsInfo.limit,
-      totalPages: sportsInfo.totalPages,
-      totalResults: sportsInfo.totalResults,
-    },
-    total,
-    msg: "market report!",
+  return {
+    report: { results: reportSport, page: sportsInfo.page, limit: sportsInfo.limit, totalPages: sportsInfo.totalPages, totalResults: sportsInfo.totalResults },
+    total, msg: "market report!"
   };
-
-  return sendObject;
 }
 
-module.exports = {
-  payload,
-  handler,
-  auth: true,
-};
+module.exports = { payload, handler, auth: true };
